f5e25d35a91d7174ec7acff99f3cf816e33e7086
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-26 07:34:59 +0100

    Implemented GetByID on '/api/zoos/:id'

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ server.post('/api/zoos', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ 
-        error: "Some useful error message" 
+        error: "Some useful error message. Your post request is no good" 
       });
     });
 });
@@ -36,12 +36,25 @@ server.get('/api/zoos', (req, res) => {
     })
     .catch(() => {
       res.status(500).json({ 
-        error: "Some useful error message" 
+        error: "Some useful error message, since you suck at making get requests" 
       })
     })
 })
 
 //GET BY ID
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params
+  console.log(id)
+  db.select().from('zoos').where({ id: id })
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(() => {
+      res.status(500).json({ 
+        error: "Some useful error message, since you suck at making get by ID requests" 
+      })
+    })
+})
 
 const port = 3300;
 server.listen(port, function() {
