e740b6c63329221f2dee00e33960403b74ac374e
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-26 07:30:12 +0100

    Added POST and GET

--- index.js
+++ index.js
@@ -2,21 +2,46 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+
 const server = express();
 
-// Define the database
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './data/sqlite3',
-  },
-  useNullAsDefault: true,
-});
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// POST
+server.post('/api/zoos', (req, res) => {
+  const name = req.body;
+  console.log(name)
+  db.insert(name)
+    .into('zoos')
+    .then(data => {
+      res.status(201).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({ 
+        error: "Some useful error message" 
+      });
+    });
+});
+
+// GET ALL
+server.get('/api/zoos', (req, res) => {
+  db.select().from('zoos')
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(() => {
+      res.status(500).json({ 
+        error: "Some useful error message" 
+      })
+    })
+})
+
+//GET BY ID
 
 const port = 3300;
 server.listen(port, function() {
