da8b3092c0fe910ac100ebb93d25297f4c3e3e4f
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-26 06:59:46 +0100

    Database linked to index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
 const server = express();
+
+// Define the database
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './data/sqlite3',
+  },
+  useNullAsDefault: true,
+});
 
 server.use(express.json());
 server.use(helmet());
